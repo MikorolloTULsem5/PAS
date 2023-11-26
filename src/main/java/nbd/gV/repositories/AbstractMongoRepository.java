9131008717758c92e8bf0babf3a0f200b0da9dcc
Mikorollo2608 <michal.p2608@wp.pl>
2023-11-26 15:36:45 +0100

    Small change for ConnectionString

--- src/main/java/nbd/gV/repositories/AbstractMongoRepository.java
+++ src/main/java/nbd/gV/repositories/AbstractMongoRepository.java
@@ -28,7 +28,7 @@ public abstract class AbstractMongoRepository<T> implements AutoCloseable {
 
     private final ConnectionString connectionString = new ConnectionString(
 //            "mongodb://localhost:27017,localhost:27018,localhost:27019/?replicaSet=replica_set_single"
-            "mongodb+srv://Michal:Michalpas@atlascluster.pweqkng.mongodb.net/"
+            "mongodb+srv://Michal:ZvDI3RNUGeTKjHTU@atlascluster.pweqkng.mongodb.net/"
     );
     private final MongoCredential credential = MongoCredential.createCredential("admin", "admin",
             "adminpassword".toCharArray());
