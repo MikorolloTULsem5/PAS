5b8542428213c617ee82d1537e2ba87616ed95d0
Mikorollo2608 <michal.p2608@wp.pl>
2023-11-28 23:50:09 +0100

    Dev test v1

--- src/test/java/integrationtests/UserControllerTests.java
+++ src/test/java/integrationtests/UserControllerTests.java
@@ -9,19 +9,20 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserControllerTests {
 
     @Test
-    public void getAllClientsTest() throws URISyntaxException {
+    void getAllClientsTest() throws URISyntaxException {
         RequestSpecification request = RestAssured.given();
 //        request.relaxedHTTPSValidation();
         Response response = request.get(new URI("http://localhost:8080/CourtRent-1.0-SNAPSHOT/api/users"));
         String responseString = response.asString();
 
         //First Client
-        assertTrue(responseString.contains("\"archive\":\"false\""));
+        assertTrue(responseString.contains("\"archive\":false"));
         assertTrue(responseString.contains("\"id\":\""));
         assertTrue(responseString.contains("\"login\":\"siemaszka\""));
         assertTrue(responseString.contains("\"clientTypeName\":\"normal\""));
@@ -29,7 +30,7 @@ public class UserControllerTests {
         assertTrue(responseString.contains("\"lastName\":\"Smith\""));
 
         //Third Client
-        assertTrue(responseString.contains("\"archive\":\"false\""));
+        assertTrue(responseString.contains("\"archive\":false"));
         assertTrue(responseString.contains("\"id\":\""));
         assertTrue(responseString.contains("\"login\":\"michas13\""));
         assertTrue(responseString.contains("\"clientTypeName\":\"coach\""));
@@ -37,5 +38,15 @@ public class UserControllerTests {
         assertTrue(responseString.contains("\"lastName\":\"Pi\""));
 
         assertEquals(200, response.getStatusCode());
+    }
+
+    @Test
+    void getAllClientsTestNoCont() throws URISyntaxException {
+        RequestSpecification request = RestAssured.given();
+        Response response = request.get(new URI("http://localhost:8080/CourtRent-1.0-SNAPSHOT/api/users"));
+        String responseString = response.asString();
+
+        assertNull(responseString);
+        assertEquals(204, response.getStatusCode());
     }
 }
