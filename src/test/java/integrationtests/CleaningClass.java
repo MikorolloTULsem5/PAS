891589a7c1d2d42e5e25e3936f73a38bbb4c1c65
Mikorollo2608 <michal.p2608@wp.pl>
2023-11-29 00:15:43 +0100

    Test test with cleaning DB

--- src/test/java/integrationtests/CleaningClass.java
+++ src/test/java/integrationtests/CleaningClass.java
@@ -1,29 +1,29 @@
-package integrationtests;
-
-import com.mongodb.client.model.Filters;
-import nbd.gV.data.datahandling.dto.ClientDTO;
-import nbd.gV.data.datahandling.dto.ReservationDTO;
-import nbd.gV.data.repositories.CourtMongoRepository;
-import nbd.gV.data.repositories.ReservationMongoRepository;
-import nbd.gV.data.repositories.UserMongoRepository;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import java.util.UUID;
-
-public class CleaningClass {
-    static final ReservationMongoRepository reservationRepository = new ReservationMongoRepository();
-    static final UserMongoRepository clientRepository = new UserMongoRepository();
-    static final CourtMongoRepository courtRepository = new CourtMongoRepository();
-
-    @BeforeAll
-    static void cleanDB() {
-        reservationRepository.getDatabase().getCollection(reservationRepository.getCollectionName(),
-                ReservationDTO.class).deleteMany(Filters.empty());
-        clientRepository.readAll(ClientDTO.class).forEach((mapper) -> clientRepository.delete(UUID.fromString(mapper.getId())));
-        courtRepository.readAll().forEach((mapper) -> courtRepository.delete(UUID.fromString(mapper.getCourtId())));
-    }
-
-    @Test
-    void test() {}
-}
+//package integrationtests;
+//
+//import com.mongodb.client.model.Filters;
+//import nbd.gV.data.datahandling.dto.ClientDTO;
+//import nbd.gV.data.datahandling.dto.ReservationDTO;
+//import nbd.gV.data.repositories.CourtMongoRepository;
+//import nbd.gV.data.repositories.ReservationMongoRepository;
+//import nbd.gV.data.repositories.UserMongoRepository;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.UUID;
+//
+//public class CleaningClass {
+//    static final ReservationMongoRepository reservationRepository = new ReservationMongoRepository();
+//    static final UserMongoRepository clientRepository = new UserMongoRepository();
+//    static final CourtMongoRepository courtRepository = new CourtMongoRepository();
+//
+//    @BeforeAll
+//    static void cleanDB() {
+//        reservationRepository.getDatabase().getCollection(reservationRepository.getCollectionName(),
+//                ReservationDTO.class).deleteMany(Filters.empty());
+//        clientRepository.readAll(ClientDTO.class).forEach((mapper) -> clientRepository.delete(UUID.fromString(mapper.getId())));
+//        courtRepository.readAll().forEach((mapper) -> courtRepository.delete(UUID.fromString(mapper.getCourtId())));
+//    }
+//
+//    @Test
+//    void test() {}
+//}
--- src/test/java/integrationtests/UserControllerTests.java
+++ src/test/java/integrationtests/UserControllerTests.java
@@ -1,18 +1,58 @@
 package integrationtests;
 
+import com.mongodb.client.model.Filters;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import jakarta.inject.Inject;
+import nbd.gV.data.datahandling.dto.ClientDTO;
+import nbd.gV.data.datahandling.dto.ReservationDTO;
+import nbd.gV.data.repositories.CourtMongoRepository;
+import nbd.gV.data.repositories.ReservationMongoRepository;
+import nbd.gV.data.repositories.UserMongoRepository;
+import nbd.gV.restapi.services.userservice.ClientService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserControllerTests {
+    @Inject
+    ClientService clientServiceTest;
+    @Inject
+    ReservationMongoRepository reservationRepository;
+    @Inject
+    UserMongoRepository clientRepository;
+    @Inject
+    CourtMongoRepository courtRepository;
+
+    void init() {
+        clientServiceTest.registerClient("Adam", "Smith", "siemaszka", "normal");
+        clientServiceTest.registerClient("Eva", "Braun", "siemaszka13", "athlete");
+        clientServiceTest.registerClient("Michal", "Pi", "michas13", "coach");
+    }
+
+    @BeforeAll
+    @AfterAll
+    void clean() {
+        reservationRepository.getDatabase().getCollection(reservationRepository.getCollectionName(),
+                ReservationDTO.class).deleteMany(Filters.empty());
+        clientRepository.readAll(ClientDTO.class).forEach((mapper) -> clientRepository.delete(UUID.fromString(mapper.getId())));
+        courtRepository.readAll().forEach((mapper) -> courtRepository.delete(UUID.fromString(mapper.getCourtId())));
+    }
+
+    @BeforeEach
+    void cleanDatabase() {
+        clean();
+        init();
+    }
 
     @Test
     void getAllClientsTest() throws URISyntaxException {
@@ -46,7 +86,12 @@ public class UserControllerTests {
         Response response = request.get(new URI("http://localhost:8080/CourtRent-1.0-SNAPSHOT/api/users"));
         String responseString = response.asString();
 
-        assertNull(responseString);
+        assertTrue(responseString.isEmpty());
         assertEquals(204, response.getStatusCode());
+    }
+
+    @Test
+    void createClientTestPos() {
+
     }
 }
