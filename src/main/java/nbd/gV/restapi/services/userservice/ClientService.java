5a28978975bd86573a3054096474f1835110c73a
Mikorollo2608 <michal.p2608@wp.pl>
2023-11-29 23:40:28 +0100

    Repair login confilict when modifying Clientv2

--- src/main/java/nbd/gV/restapi/services/userservice/ClientService.java
+++ src/main/java/nbd/gV/restapi/services/userservice/ClientService.java
@@ -78,7 +78,7 @@ public class ClientService extends UserService {
     public void modifyClient(Client modifiedClient) {
         if (!userRepository.read(Filters.and(
                 Filters.eq("login", modifiedClient.getLogin()),
-                Filters.not(Filters.eq("_id", modifiedClient.getId())))).isEmpty()) {
+                Filters.not(Filters.eq("_id", modifiedClient.getId()))), ClientDTO.class).isEmpty()) {
             throw new UserLoginException("Nie udalo sie zmodyfikowac podanego klienta - proba zmiany loginu na login wystepujacy juz u innego klienta");
         }
         if (!userRepository.updateByReplace(modifiedClient.getId(), ClientMapper.toMongoUser(modifiedClient))) {
