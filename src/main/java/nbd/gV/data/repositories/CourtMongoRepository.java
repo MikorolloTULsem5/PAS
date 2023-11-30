bdc35b5fa50b72bd9354ac492d21d3d24456eecd
Mikorollo2608 <michal.p2608@wp.pl>
2023-11-30 22:20:18 +0100

    Big fight

--- src/main/java/nbd/gV/data/repositories/CourtMongoRepository.java
+++ src/main/java/nbd/gV/data/repositories/CourtMongoRepository.java
@@ -5,12 +5,15 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.CreateCollectionOptions;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.ValidationOptions;
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.ForbiddenException;
 import nbd.gV.data.datahandling.dto.CourtDTO;
 import nbd.gV.data.datahandling.dto.ReservationDTO;
+import nbd.gV.data.datahandling.mappers.CourtMapper;
 import nbd.gV.exceptions.CourtException;
 import nbd.gV.exceptions.MyMongoException;
+import nbd.gV.model.courts.Court;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
@@ -91,5 +94,14 @@ public class CourtMongoRepository extends AbstractMongoRepository<CourtDTO> {
         } finally {
             clientSession.close();
         }
+    }
+
+    @PostConstruct
+    private void init() {
+        create(CourtMapper.toMongoCourt(new Court(UUID.fromString("634d9130-0015-42bb-a70a-543dee846760"), 100, 100, 1)));
+        create(CourtMapper.toMongoCourt(new Court(UUID.fromString("fe6a35bb-7535-4c23-a259-a14ac0ccedba"),100, 200, 2)));
+        create(CourtMapper.toMongoCourt(new Court(UUID.fromString("30ac2027-dcc8-4af7-920f-831b51023bc9"),300, 200, 3)));
+        create(CourtMapper.toMongoCourt(new Court(UUID.fromString("d820d682-0f5d-46b7-9963-66291e5f64b0"),350, 100, 4)));
+        create(CourtMapper.toMongoCourt(new Court(UUID.fromString("2e9258b2-98dd-4f9a-8f73-6f4f56c2e618"),150, 200, 5)));
     }
 }
