1c7172c2b64db975d9a60b7b354979d175e06625
Mikorollo2608 <michal.p2608@wp.pl>
2024-01-14 18:39:54 +0100

    JSF WORKINGGG

--- MVC/src/main/java/nbd/gv/mvc/AppMVC.java
+++ MVC/src/main/java/nbd/gv/mvc/AppMVC.java
@@ -1,14 +1,21 @@
 package nbd.gv.mvc;
 
+import jakarta.faces.webapp.FacesServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppMVC {
 
     public static void main(String[] args) {
-        //Gdyby nie zaczytalo z .properties mozna odkomentowac
-//        System.setProperty("server.servlet.context-path", "/api");
         SpringApplication.run(AppMVC.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean<?> servletRegistrationBean() {
+        FacesServlet servlet = new FacesServlet();
+        return new ServletRegistrationBean<>(servlet, "*.jsf");
     }
 }
---
+++ MVC/src/main/java/nbd/gv/mvc/config/RedirectController.java
@@ -0,0 +1,17 @@
+package nbd.gv.mvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RedirectController implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("redirect:/layout.xhtml");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+}
